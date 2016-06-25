92cb00525682a31dd251ac187beeb78bacf4b88e
Jonathan S. Fisher <jfisher@tomitribe.com>
2016-06-25 14:31:35 -0500

    update context root update gitignore add setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "setting exports..."
+
+export project_twitter_oauth_consumer_key=yee yee
+export project_twitter_oauth_consumer_secret=yee yee
+export project_twitter_oauth_access_token=yee yee
+export project_twitter_oauth_access_token_secret=yee yee
+export github_atoken="yee yee"
+
+echo "done"
