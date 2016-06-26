fb216aaec757d75c685e8ce6d735583558362382
Thiago Veronezi <thiago@veronezi.org>
2016-06-26 10:07:12 -0400

    several minor visual adjustments.

--- src/main/static/gulpfile.js
+++ src/main/static/gulpfile.js
@@ -110,8 +110,7 @@ gulp.task('js-third-party', function () {
         './bower_components/ngstorage/ngStorage.min.js',
         './bower_components/angular-cookies/angular-cookies.min.js',
         './bower_components/angular-resource/angular-resource.min.js',
-        './node_modules/underscore.string/dist/underscore.string.min.js',
-        './bower_components/particles.js/particles.js'
+        './node_modules/underscore.string/dist/underscore.string.min.js'
     ]).pipe(concat('_.js')).pipe(gulp.dest('../../../target/static-resources/app/third-party/'));
 });
 
