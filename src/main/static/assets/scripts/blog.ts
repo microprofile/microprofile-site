4941f0b37c38f03150bde29a81d86b6dc2107e51
Thiago Veronezi <thiago@veronezi.org>
2016-09-15 20:11:22 -0400

    move adoc title to page title; remove anchors.

--- src/main/static/assets/scripts/blog.ts
+++ src/main/static/assets/scripts/blog.ts
@@ -12,12 +12,19 @@ angular.module('microprofileio-blog', [])
         return {
             restrict: 'A',
             scope: {
-                resource: '='
+                resource: '=',
+                title: '='
             },
             templateUrl: 'app/templates/dir_blog_entry.html',
             controller: ['$scope', '$timeout', 'microprofileioBlogService', '$sce', ($scope, $timeout, srv, $sce) => {
                 srv.getHtml($scope.resource).then((response) => $timeout(() => $scope.$apply(() => {
-                    $scope.content = $sce.trustAsHtml(response.data.content);
+                    let content = angular.element(response.data.content);
+                    let title = content.find('h1').first();
+                    title.find('> *').remove();
+                    $scope.title = title.html();
+                    title.remove();
+                    content.find('.anchor').remove();
+                    $scope.content = $sce.trustAsHtml(content.html());
                 })));
             }]
         };
